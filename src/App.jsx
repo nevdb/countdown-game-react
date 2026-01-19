8361bba157ad5b27cdf36dfd806f944e2259be52
nevena <dnevena27@gmail.com>
2026-01-19 15:42:24 +0200

    chnage player name on known approach

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <>
+      <h1>Countdown Game</h1>
+      <Player />
+      <div id="challenges"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? playerName : "unknown entity"}</h2>
+      <p>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}> Set Name</button>
+      </p>
+    </section>
+  );
+}
